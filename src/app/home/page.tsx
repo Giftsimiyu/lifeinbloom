64ca54d0fba43a3a014d72e64f9cfbd9fc1fecf3
Giftsimiyu <simiyugift33@gmail.com>
2026-05-22 05:37:46 +0300

    fixed error

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const revalidate = 60;
+
 import React, { useEffect, useState } from "react";
 import PostCard from "../components/postCard";
 import QuestionOfTheWeek from "../components/questionOfTheWeek";
